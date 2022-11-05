b1f3ef866a90688049f8655ed60e2cb032fb36cc
smerch88 <maxarmyk@gmail.com>
2022-11-05 18:08:52 +0200

    save

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 "use strict";
 
-const subs = document.querySelectorAll(".js-subs");
-const refreshButtonRef = document.querySelector(".refresh");
-// console.log(subs);
-
-subsData = {
+const subsData = {
   twitter: 189000,
   youtube: 335000,
   instagram: 189000,
   tiktok: 189000,
 };
 
-const refreshCounter = (data) => {
-  const { twitter, youtube, instagram, tiktok } = data;
+const subs = document.querySelectorAll(".js-subs");
+const refreshButtonRef = document.querySelector(".refresh");
+
+const handleClick = () => {
+  const { twitter, youtube, instagram, tiktok } = subsData;
   console.log(twitter);
   console.log(youtube);
   console.log(instagram);
   console.log(tiktok);
+  subs[0].textContent = twitter;
+  subs[1].textContent = youtube;
+  subs[2].textContent = instagram;
+  subs[3].textContent = tiktok;
+  subs.forEach((item) => (item.textContent = subsData.item));
 };
 
-// subs.forEach((e) => console.log(e.textContent));
-
-refreshButtonRef.addEventListener("click", refreshCounter(subsData));
+refreshButtonRef.addEventListener("click", handleClick);
