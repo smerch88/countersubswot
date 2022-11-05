54b6b2b035af8e05e6d3565146b6e3bfb37a6d4e
smerch88 <maxarmyk@gmail.com>
2022-11-05 17:41:24 +0200

    save

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const subs = document.querySelectorAll(".js-subs");
 const refreshButtonRef = document.querySelector(".refresh");
-console.log(subs);
+// console.log(subs);
 
-subsData = [
-  { twitter: 189000 },
-  { youtube: 335000 },
-  { instagram: 189000 },
-  { tiktok: 189000 },
-];
+subsData = {
+  twitter: 189000,
+  youtube: 335000,
+  instagram: 189000,
+  tiktok: 189000,
+};
 
-subs.forEach((e) => console.log(e.textContent));
+const refreshCounter = (data) => {
+  const { twitter, youtube, instagram, tiktok } = data;
+  console.log(twitter);
+  console.log(youtube);
+  console.log(instagram);
+  console.log(tiktok);
+};
+
+// subs.forEach((e) => console.log(e.textContent));
+
+refreshButtonRef.addEventListener("click", refreshCounter(subsData));
