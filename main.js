e8870f10df748afb57640c4ec5d7e35907b52827
smerch88 <maxarmyk@gmail.com>
2022-11-05 22:09:56 +0200

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const subsData = {
+let subsData = {
   twitter: 200000,
   youtube: 350000,
   instagram: 140000,
@@ -15,6 +15,7 @@ const instagramStages = document.querySelectorAll(".stage-instagram");
 const tiktokStages = document.querySelectorAll(".stage-tiktok");
 
 const handleClick = () => {
+  fetchYouTube();
   const { twitter, youtube, instagram, tiktok } = subsData;
   console.log(twitter);
   console.log(youtube);
@@ -53,3 +54,15 @@ function checkStage() {
       : (e.classList.remove("passed"), e.classList.add("not-passed"));
   });
 }
+
+async function fetchYouTube() {
+  //Отримуємо кілкість підписок з ютуба
+  const response = await fetch(
+    "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCh554z2-7vIA-Mf9qAameoA&key=AIzaSyDPPYTQRQayT5Rj34Esbjr68jjfYvmLl0M"
+  ).then((res) => res.json()); //Перетворюємо дані в json і повертаємо в response
+  const subscriber = response.items[0].statistics.subscriberCount; //З response находимо і записуємо кількість підписок
+  console.log(subscriber);
+  return (subsData.youtube = subscriber); //Повертаємо результат
+}
+
+fetchYouTube();
